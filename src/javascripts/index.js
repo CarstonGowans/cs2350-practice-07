cba430699eeb6ddf389a3803faf91bc661dd10cd
BuildTools <unconfigured@null.spigotmc.org>
2020-03-27 20:23:24 -0600

    Final Submission

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,71 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import "bootstrap"
+
+const displayRandomBox = function() { //We use Const when we define functions.
+    let r = Math.random()
+    if(r < .5) {
+        document.write('<div class="red box"></div>')
+    }
+    else {
+        document.write('<div class="blue box"></div>')
+    }
+}
+
+
+function DispalyNBoxesUsingFor(n) {
+    for(let i = 0; i < n; i++) {
+        displayRandomBox();
+      }
+}
+
+function DispalyNBoxesUsingWhile(n) {
+    let j = 0;
+    while(j < n) {
+    displayRandomBox();
+    
+        j++;
+    }
+}
+
+function DispalyNBoxesUsingDoWhile(n) {
+    let k = 0;
+    do {
+    displayRandomBox();
+    
+        k++;
+    } while (k < n);
+    
+}
+
+DispalyNBoxesUsingFor(15);
+DispalyNBoxesUsingWhile(12);
+DispalyNBoxesUsingDoWhile(14);
+
+
+const square = function(x) {
+    return x * x;
+}
+
+alert(square(10));
+
+
+document.getElementById("Function").addEventListener("click", displayMessage);
+
+function displayMessage() {
+    var Input = document.getElementById("NameField").value;
+    alert(Input);
+}
+
+// while(k = 0) {
+//     let r = Math.random()
+//     if(r < .5) {
+//         document.write('<div class="red box"></div>')
+//     }
+//     else {
+//         document.write('<div class="blue box"></div>')
+//     }
+
+
+// }
